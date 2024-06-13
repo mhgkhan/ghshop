2fe35409a8a07d9eacf4aaa8caa8483211e5c91a
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-06-13 17:44:34 +0500

    footer completed, install react-icons packege

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1></h1>;
+  return <h1 className="text-3xl text-black">hello world</h1>;
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
 import React from "react";
+import { FaDiscord, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-green-700 w-full">
+      <div className="container mx-auto p-5 flex items-center justify-between gap-3 flex-wrap-reverse">
+        <p className="text-white">
+          | Copyright&copy; Allright reserved GHSHOP 2024
+        </p>
+        <div className="icons-social flex items-center justify-center gap-2 text-white text-2xl">
+          <Link href={"/"}>
+            <FaDiscord />
+          </Link>
+          <Link href={"/"}>
+            <FaLinkedin />
+          </Link>
+          <Link href={"/"}>
+            <FaWhatsapp />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
