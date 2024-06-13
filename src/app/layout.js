00dfb21fe89e824efc0d826d001b747cc0abf706
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-06-13 09:23:28 +0500

    Added Fonts for headings, and for contents

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,12 @@
-import { Inter } from "next/font/google";
+import { Magra, Roboto_Flex } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoFlex = Roboto_Flex({ subsets: ["latin"], weight: ["300", "400"] });
+
+export const magraFontForHeading = Magra({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={robotoFlex.className}>{children}</body>
     </html>
   );
 }
