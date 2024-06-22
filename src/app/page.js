04a8d570fdfa4325213722f9f29b490635c29858
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-06-22 22:04:38 +0500

    Change Content To Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl text-black">hello world</h1>;
+  return (
+    <h1 className="text-3xl text-black text-center p-5">
+      Hi, <br /> Muhammad Hasnain Developer{" "}
+    </h1>
+  );
 }
