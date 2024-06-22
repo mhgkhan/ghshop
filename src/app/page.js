c97f4d4bba499f18fc531a009c10166d5e674ac7
Muhammad Hasnain <muhammadhasnainghazna@gmail.com>
2024-06-22 22:14:47 +0500

    Change Home Page Content

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import { FaMailBulk } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl text-black text-center p-5">
-      Hi, <br /> Muhammad Hasnain Developer{" "}
+    <h1
+      className="text-xl text-black text-center p-5"
+      style={{ lineHeight: "2" }}
+    >
+      Hi, <br /> This Website is Under Construction <br /> <br /> Have any issue
+      In Structure, Theme etc ? Please Contact with us <br />
+      <a
+        href="mailto:muhammadhasnainghazna@gmail.com"
+        className="text-white bg-blue-500 rounded-md p-2 m-1"
+      >
+        Message me
+      </a>
     </h1>
   );
 }
